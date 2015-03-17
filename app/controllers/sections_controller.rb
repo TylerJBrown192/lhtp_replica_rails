6856d0c4321dd0a6f5d32c3228d9d2d1c6e2e749
Lee Jorgensen and Tyler Brown <leejorgensen22@gmail.com>
2015-03-17 11:24:03 -0700

    Build out inital file structure and add views and partials to sections

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -2,4 +2,34 @@ class SectionsController < ApplicationController
   def index
     @sections = Section.all
   end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def new
+    @section = Section.new
+  end
+
+  def create
+    @section = Section.new(params[:section])
+    if @section.save
+      redirect_to sections_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    if @section.update(params[:section])
+      redirect_to sections_path
+    else
+      render :edit
+    end
+  end
 end
